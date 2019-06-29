60c6756b530eb8479a8b99c5dba635ae5edf532e
大辰沉 <xueliqiang1988@sina.com>
2019-06-29 23:59:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ echo 'pull from git'
 git pull origin master
 echo 'pull from git done!'
 echo 'start building'
-/root/.nvm/versions/node/v8.11.1/bin/node/npm run build
+export PATH=/root/.nvm/versions/node/v8.11.1/bin:$PATH
+npm run build
 echo 'travis build done!'
