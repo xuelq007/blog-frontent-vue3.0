05c0e0bbfe4281cd3ba99327713c55c85d7f0703
xuelq007 <xueliqiang1988@sina.com>
2019-06-29 16:24:08 +0800

    test travis

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /usr/blog/blog-frontent-vue3.0
+echo 'pull from git'
 git pull origin master
+echo 'pull from git done!'
+echo 'start building'
 npm run build
-echo 'travis build done!
+echo 'travis build done!'
