0c208c5f7a77ba0eaeaba0b3ea7edc4bdf204898
xuelq007 <xueliqiang1988@sina.com>
2019-07-03 13:12:15 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 cd /usr/blog/blog-frontent-vue3.0
 echo 'pull from git'
 git pull origin master
 echo 'pull from git done!'
 echo 'start building'
-export PATH=/usr/bin:$PATH
 npm run build
 echo 'travis build done!'
