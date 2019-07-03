ecb9a2dc6db6e759efebb5cfbee9d817a650195c
xuelq007 <xueliqiang1988@sina.com>
2019-07-03 12:56:25 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ echo 'pull from git'
 git pull origin master
 echo 'pull from git done!'
 echo 'start building'
-/root/.nvm/versions/node/v8.11.1/bin/npm run build
+npm -v
+npm run build
 echo 'travis build done!'
