07df5fb31c88cbc701de06e8a3c170bfe6a36613
xuelq007 <xueliqiang1988@sina.com>
2019-07-03 09:27:22 +0800

    fix typo

--- src/components/home/about/About.vue
+++ src/components/home/about/About.vue
@@ -2,7 +2,7 @@
   <div id="about" class="about" v-on:click="handleClick">
     <div class="tools-wrap">
         <section class="tools-section tools-section-me" style="">
-            <div class="aboutme-wrap">现居么么哒<br>某厂前端<br>ES6, Typescript, html5, css3<br>React, Angular2, Exjts<br>Ionic3<br>Webpack, Rollup<br>java, C#<br>伪文艺青年<br>历史爱好者</div>
+            <div class="aboutme-wrap">现居上海<br>某厂前端<br>ES6, Typescript, html5, css3<br>React, Angular2, Exjts<br>Ionic3<br>Webpack, Rollup<br>java, C#<br>伪文艺青年<br>历史爱好者</div>
         </section>
     </div>
   </div>
